6ecf9e272f6c48bfcd792efa642147d959286d92
Ameerac-ami <ameerac.ami@gmail.com>
2022-10-26 10:49:30 +0530

    Initial Commit

---
+++ src/Components/screens/Navbar.js
@@ -0,0 +1,13 @@
+import React, {useState , useEffect} from 'react'
+import styled from 'styled-components'
+import { useEffect, useState } from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <h2>Nice to meet you</h2>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/Components/screens/Staticts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Staticts() {
+  return (
+    <>
+        <h1>Staticts</h1>
+    </>
+  )
+}
+
+export default Staticts
